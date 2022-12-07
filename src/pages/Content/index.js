0d4635336d4a936c1d0cf535433da5aa3655f3ab
Bobby Azarbayejani <bobby@azarbayejani.com>
2022-12-07 09:54:56 -0800

    Add ability to pick tempo ranges and reset tempo

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
 
 const appDiv = document.createElement('div');
 appDiv.id = 'pitchSliderApp';
@@ -12,17 +8,39 @@ document.querySelector('.inline_player').appendChild(appDiv);
 const App = () => {
   const audioRef = React.useRef();
   const [sliderValue, setSliderValue] = React.useState(1000);
+  const [tempoRangeIndex, setTempoRangeIndex] = React.useState(1);
+
+  const tempoRanges = [
+    {
+      label: '±6',
+      min: 940,
+      max: 1060
+    },
+    {
+      label: '±10',
+      min: 900,
+      max: 1100
+    },
+    {
+      label: '±16',
+      min: 840,
+      max: 1160,
+    },
+    {
+      label: 'WIDE',
+      min: 100,
+      max: 2000,
+    }
+  ];
+
+  const tempoRange = tempoRanges[tempoRangeIndex % tempoRanges.length];
+
   const percentage = ((sliderValue - 1000) / 1000 * 100).toPrecision(3);
   const playbackRate = 1 + ((sliderValue - 1000) / 1000);
 
   const handleSliderChange = (event) => {
     const value = event.target.value;
     setSliderValue(value);
-
-    if (!audioRef.current?.paused) {
-      audioRef.current.preservesPitch = false;
-      audioRef.current.playbackRate = 1 + ((value - 1000) / 1000);
-    }
   }
 
   useEffect(() => {
@@ -38,12 +56,20 @@ const App = () => {
     }
   }, [playbackRate]);
 
+  useEffect(() => {
+    if (!audioRef.current?.paused) {
+      audioRef.current.preservesPitch = false;
+      audioRef.current.playbackRate = 1 + ((sliderValue - 1000) / 1000);
+    }
+  }, [sliderValue]);
+
 
   return (
-    <div>
-      <input type="range" onChange={handleSliderChange} onInput={handleSliderChange} min="0" max="2000" value={sliderValue} />
-      <span>{percentage >= 0 ? '+' : ''}{percentage}%</span>
-    </div>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <input className="BandcampPitchSlider_range" type="range" onChange={handleSliderChange} onInput={handleSliderChange} min={tempoRange.min} max={tempoRange.max} value={sliderValue} />
+      <button title="Reset" className="BandcampPitchSlider_button" onClick={() => setSliderValue(1000)}>{percentage >= 0 ? '+' : ''}{percentage}%</button>{' '}
+      <button title="Range adjust" className="BandcampPitchSlider_button" onClick={() => setTempoRangeIndex(tempoRangeIndex + 1)}>({tempoRange.label})</button>
+    </div >
   );
 }
 
