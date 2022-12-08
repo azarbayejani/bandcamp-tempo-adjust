7854f9305864f7bfabe8fb5e1f81bdd022c3a150
Bobby Azarbayejani <bobby@azarbayejani.com>
2022-12-07 22:40:46 -0800

    Fix app crash on non-album pages

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,8 +4,7 @@ import PitchAdjust from './PitchAdjust';
 
 const appDiv = document.createElement('div');
 appDiv.id = 'pitchSliderApp';
-document.querySelector('.inline_player').append(appDiv);
-
+const player = document.querySelector('.inline_player');
 
 const App = () => {
   return (
@@ -15,4 +14,7 @@ const App = () => {
   );
 }
 
-ReactDOM.render(<App />, document.querySelector('#pitchSliderApp'))
+if (player) {
+  player.append(appDiv);
+  ReactDOM.render(<App />, document.querySelector('#pitchSliderApp'))
+}
