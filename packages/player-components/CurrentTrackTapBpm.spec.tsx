a4052076b3a67ff9ae305488739b0c6154d84a06
Bobby Azarbayejani <bobby@azarbayejani.com>
2024-11-16 16:20:43 -0800

    Fix CurrentTrackTapBpm specs

--- packages/player-components/CurrentTrackTapBpm.spec.tsx
+++ packages/player-components/CurrentTrackTapBpm.spec.tsx
@@ -1,10 +1,17 @@
-import { expect, describe, it, vi, afterEach } from 'vitest';
+import { expect, describe, it, vi, afterEach, beforeEach } from 'vitest';
+
 import { render, screen, waitFor, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import React from 'react';
 
 import CurrentTrackTapBpm from './CurrentTrackTapBpm';
+
+declare global {
+  var jest: {
+    advanceTimersByTime: (time: number) => void;
+  };
+}
 
 describe('CurrentTrackTapBpm', () => {
   const onSave = vi.fn();
@@ -55,84 +62,6 @@ describe('CurrentTrackTapBpm', () => {
     });
   });
 
-  it('allows you to tap the bpm and save with Enter', async () => {
-    vi.useFakeTimers();
-    const user = userEvent.setup({
-      advanceTimers: vi.advanceTimersByTime,
-    });
-
-    render(<CurrentTrackTapBpm onSave={onSave} onCancel={onCancel} />);
-
-    await waitFor(() => {
-      expect(screen.getByRole('textbox')).toHaveFocus();
-    });
-
-    await user.keyboard(' ');
-
-    vi.advanceTimersByTime(1000);
-
-    await user.keyboard(' ');
-
-    await waitFor(() => {
-      expect(screen.getByRole('textbox')).toHaveValue('60');
-    });
-
-    vi.advanceTimersByTime(500);
-    await user.keyboard(' ');
-
-    // check to see if it uses a running average
-    await waitFor(() => {
-      expect(screen.getByRole('textbox')).toHaveValue('90');
-    });
-
-    await user.keyboard('{Enter}');
-
-    await waitFor(() => {
-      expect(onSave).toHaveBeenCalledWith('90');
-    });
-
-    vi.useRealTimers();
-  });
-
-  it('allows you to tap the bpm and save with Enter', async () => {
-    vi.useFakeTimers();
-    const user = userEvent.setup({
-      advanceTimers: vi.advanceTimersByTime,
-    });
-
-    render(<CurrentTrackTapBpm onSave={onSave} onCancel={onCancel} />);
-
-    await waitFor(() => {
-      expect(screen.getByRole('textbox')).toHaveFocus();
-    });
-
-    await user.keyboard(' ');
-
-    vi.advanceTimersByTime(1000);
-
-    await user.keyboard(' ');
-
-    await waitFor(() => {
-      expect(screen.getByRole('textbox')).toHaveValue('60');
-    });
-
-    vi.advanceTimersByTime(500);
-    await user.keyboard(' ');
-
-    // check to see if it uses a running average
-    await waitFor(() => {
-      expect(screen.getByRole('textbox')).toHaveValue('90');
-    });
-
-    await user.click(screen.getByRole('button', { name: /save/ }));
-
-    await waitFor(() => {
-      expect(onSave).toHaveBeenCalledWith('90');
-    });
-
-    vi.useRealTimers();
-  });
-
   it('calls onCancel when escape is entered', async () => {
     render(<CurrentTrackTapBpm onSave={onSave} onCancel={onCancel} />);
 
@@ -158,6 +87,91 @@ describe('CurrentTrackTapBpm', () => {
 
     await waitFor(() => {
       expect(onCancel).toHaveBeenCalled();
+    });
+  });
+
+  describe('when tapping bpm', () => {
+    beforeEach(() => {
+      vi.useFakeTimers();
+      globalThis.jest = {
+        advanceTimersByTime: vi.advanceTimersByTime.bind(vi),
+      };
+    });
+
+    it('allows you to tap the bpm and save with Enter', async () => {
+      const user = userEvent.setup({
+        advanceTimers: vi.advanceTimersByTime.bind(vi),
+      });
+
+      render(<CurrentTrackTapBpm onSave={onSave} onCancel={onCancel} />);
+
+      await waitFor(() => {
+        expect(screen.getByRole('textbox')).toHaveFocus();
+      });
+
+      await user.keyboard(' ');
+
+      vi.advanceTimersByTime(1000);
+
+      await user.keyboard(' ');
+
+      await waitFor(() => {
+        expect(screen.getByRole('textbox')).toHaveValue('60');
+      });
+
+      vi.advanceTimersByTime(500);
+      await user.keyboard(' ');
+
+      // check to see if it uses a running average
+      await waitFor(() => {
+        expect(screen.getByRole('textbox')).toHaveValue('90');
+      });
+
+      await user.keyboard('{Enter}');
+
+      await waitFor(() => {
+        expect(onSave).toHaveBeenCalledWith('90');
+      });
+
+      vi.useRealTimers();
+    });
+
+    it('allows you to tap the bpm and save with Enter', async () => {
+      const user = userEvent.setup({
+        advanceTimers: vi.advanceTimersByTime,
+      });
+
+      render(<CurrentTrackTapBpm onSave={onSave} onCancel={onCancel} />);
+
+      await waitFor(() => {
+        expect(screen.getByRole('textbox')).toHaveFocus();
+      });
+
+      await user.keyboard(' ');
+
+      vi.advanceTimersByTime(1000);
+
+      await user.keyboard(' ');
+
+      await waitFor(() => {
+        expect(screen.getByRole('textbox')).toHaveValue('60');
+      });
+
+      vi.advanceTimersByTime(500);
+      await user.keyboard(' ');
+
+      // check to see if it uses a running average
+      await waitFor(() => {
+        expect(screen.getByRole('textbox')).toHaveValue('90');
+      });
+
+      await user.click(screen.getByRole('button', { name: /save/ }));
+
+      await waitFor(() => {
+        expect(onSave).toHaveBeenCalledWith('90');
+      });
+
+      vi.useRealTimers();
     });
   });
 });
