caadad9e49be123b8e725f5c4771e79006fb664b
Bobby Azarbayejani <bobby@azarbayejani.com>
2024-11-19 11:40:34 -0800

    Add Firefox uuid

--- apps/discogs-tempo-adjust/wxt.config.ts
+++ apps/discogs-tempo-adjust/wxt.config.ts
@@ -3,10 +3,15 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   manifest: {
     name: 'Discogs Tempo Adjust',
-    version: '0.1.0',
+    version: '0.1.1',
     description:
       'A browser extension to detect and adjust track tempo on Discogs',
     action: {},
+    browser_specific_settings: {
+      gecko: {
+        id: '{6fc0284b-f220-4709-acad-d8b95f55637f}',
+      },
+    },
   },
   modules: ['@wxt-dev/module-react', '@tempo-adjust/wxt-hooks'],
   srcDir: 'src',
