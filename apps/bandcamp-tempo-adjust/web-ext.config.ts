018cfbc09e3b89b805f8156ec6b69d76e189d1ca
Bobby Azarbayejani <bobby@azarbayejani.com>
2025-06-06 13:14:57 -0700

    Fix dev target on latest chrome

--- apps/bandcamp-tempo-adjust/web-ext.config.ts
+++ apps/bandcamp-tempo-adjust/web-ext.config.ts
@@ -2,4 +2,6 @@ import { defineRunnerConfig } from 'wxt';
 
 export default defineRunnerConfig({
   startUrls: ['https://illegalafterstracks.bandcamp.com'],
+  // TODO: Remove this when upgrading to latest wxt
+  chromiumArgs: ['--disable-features=DisableLoadExtensionCommandLineSwitch'],
 });
--- apps/discogs-tempo-adjust/web-ext.config.ts
+++ apps/discogs-tempo-adjust/web-ext.config.ts
@@ -4,4 +4,6 @@ export default defineRunnerConfig({
   startUrls: [
     'https://www.discogs.com/release/15571627-Sketch-Artist-illegal-afters-01',
   ],
+  // TODO: Remove this when upgrading to latest wxt
+  chromiumArgs: ['--disable-features=DisableLoadExtensionCommandLineSwitch'],
 });
